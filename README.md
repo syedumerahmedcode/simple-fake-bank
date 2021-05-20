01030eb763c3166d556c60fbe52092bd0120d20d
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-05-20 16:31:04 +0200

    simüple-fake-bank: Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple-fake-bank
---
+++ src/main/java/com/umer/simplefakebank/SimpleFakeBankApplication.java
@@ -0,0 +1,13 @@
+package com.umer.simplefakebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFakeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFakeBankApplication.class, args);
+	}
+
+}
