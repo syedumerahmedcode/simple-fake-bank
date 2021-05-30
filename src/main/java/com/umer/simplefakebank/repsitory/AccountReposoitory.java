bfc6dd4a728bb9ea3ad52ce744acfb798e7dfa56
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-05-30 19:27:11 +0200

    simple-fake-bank: Added Account Respoitory.

--- src/main/java/com/umer/simplefakebank/repsitory/AccountReposoitory.java
+++ src/main/java/com/umer/simplefakebank/repsitory/AccountReposoitory.java
@@ -2,9 +2,9 @@ package com.umer.simplefakebank.repsitory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.umer.simplefakebank.entities.User;
+import com.umer.simplefakebank.entities.Account;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long	>{
+public interface AccountReposoitory extends JpaRepository<Account, Long> {
 
 }
