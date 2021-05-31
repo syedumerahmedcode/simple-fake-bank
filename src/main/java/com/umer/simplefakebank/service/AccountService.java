f5f72d92d4788fb3de12480ac5cd3362453dc9cf
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-05-31 20:47:30 +0200

    simple-fake-bank: Added transfer method in Account resvice - wip.

--- src/main/java/com/umer/simplefakebank/service/AccountService.java
+++ src/main/java/com/umer/simplefakebank/service/AccountService.java
@@ -21,22 +21,39 @@ public class AccountService {
 	private final AccountReposoitory accountReposoitory;
 	private final UserRepository userRepository;
 
-	//TODO: Further improvements:
+	// TODO: Further improvements:
 	/*
-	 * 1) Create mappers that contain user infomration 
-	 * 2) Read Request DTO from Controller
-	 * 3) Create a response mapper 
-	 * 4) Return response mapper from this method to the controller
+	 * 1) Create mappers that contain user infomration 2) Read Request DTO from
+	 * Controller 3) Create a response mapper 4) Return response mapper from this
+	 * method to the controller
 	 */
 	public void createNewAccount(Long userId) {
 		log.debug("Creatging a new account - {}", userId);
 
 		// Get User from the Id
 		Optional<User> user = userRepository.findById(userId);
-		Account account=Account.builder().id(1L).initialDepositAmount(BigDecimal.valueOf(100L)).build();
-		account= accountReposoitory.save(account);
-		
+		Account account = Account.builder().id(1L).initialDepositAmount(BigDecimal.valueOf(100L)).build();
+		account = accountReposoitory.save(account);
+
 		log.debug("Created Account - {}", account);
+	}
+
+	public void transfer(Account senderAccount, Account receiverAccount, BigDecimal value) {
+		log.debug("Starting transfer from sender account:[{}] to receiver account:[{}] --> amount[{}]", senderAccount,
+				receiverAccount, value);
+		//TODO: Do error check first before executing the transfer
+		
+		// Update account values
+		senderAccount.setBalance(senderAccount.getBalance().min(value));
+		receiverAccount.setBalance(receiverAccount.getBalance().add(value));
+		
+		// save new account information
+		accountReposoitory.save(senderAccount);
+		accountReposoitory.save(receiverAccount);
+		
+
+		log.debug("Finished transfer from sender account:[{}] to receiver account:[{}] --> amount[{}]", senderAccount,
+				receiverAccount, value);
 	}
 
 }
