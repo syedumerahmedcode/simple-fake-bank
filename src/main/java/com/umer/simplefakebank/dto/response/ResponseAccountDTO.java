7c8e8156e37739c2c7cd41af44c88f0f639e60d9
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-10 21:18:56 +0200

    simple-fake-bank: Added ApiModel and ApiModelProperty to ResponseAccountDTO.

--- src/main/java/com/umer/simplefakebank/dto/response/ResponseAccountDTO.java
+++ src/main/java/com/umer/simplefakebank/dto/response/ResponseAccountDTO.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +16,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ApiModel(value = "New account created response.", description = "The new account information.")
 public class ResponseAccountDTO {
-	
+
 	@JsonProperty("user_id")
+	@ApiModelProperty(notes = "The Id of the customer of the owner of the account.")
 	private Long userId;
-	
+
 	@JsonProperty("account_id")
+	@ApiModelProperty(notes = "The account Id.")
 	private Long id;
-	
+
 	@JsonProperty("balance")
+	@ApiModelProperty(notes = "The balance of the account.")
 	private BigDecimal balance;
-	
+
 	@JsonProperty("created_at")
+	@ApiModelProperty(notes = "The date and time when the account was created.")
 	private LocalDateTime creationTimestamp;
 
 }
