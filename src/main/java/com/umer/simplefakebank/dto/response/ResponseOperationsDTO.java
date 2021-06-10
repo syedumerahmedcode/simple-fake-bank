e5b17a4f293a396e8f4699782817c951d6bddd17
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-10 22:06:59 +0200

    simple-fake-bank: Added ResponseOperationsDTO which holds all the operations for one given account Id.

--- src/main/java/com/umer/simplefakebank/dto/response/ResponseOperationsDTO.java
+++ src/main/java/com/umer/simplefakebank/dto/response/ResponseOperationsDTO.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,16 +17,19 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
+@ApiModel(value = "List of operations response.", description = "List of operations collected for the given account Id.")
 public class ResponseOperationsDTO {
 	
 	@JsonProperty("account_id")
+	@ApiModelProperty(notes = "The id of the account.")
 	private Long accountId;
 	
 	@JsonProperty("operations")
+	@ApiModelProperty(notes = "List of operations for the given account Id.")
 	private List<ResponseOperationDTO> operationDTOList=new ArrayList<>();
 	
 	@JsonProperty("created_at")
+	@ApiModelProperty(notes = "Date and time when operation was collected.")
 	private LocalDateTime creationTimestamp;
 
 }
