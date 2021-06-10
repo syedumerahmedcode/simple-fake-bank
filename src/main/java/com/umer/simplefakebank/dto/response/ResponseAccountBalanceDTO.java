3edfa2b5c2318498119ec2e2fbd629f5450bc0ec
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-10 21:32:35 +0200

    simple-fake-bank: Added ResponseAccountBalanceDTO.

---
+++ src/main/java/com/umer/simplefakebank/dto/response/ResponseAccountBalanceDTO.java
@@ -0,0 +1,34 @@
+package com.umer.simplefakebank.dto.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ApiModel(value = "Account balance response.", description = "The balance of an account.")
+public class ResponseAccountBalanceDTO {
+
+	@JsonProperty("account_id")
+	@ApiModelProperty(notes = "The Account Id.")
+	private Long id;
+
+	@JsonProperty("balance")
+	@ApiModelProperty(notes = "Balance of an account.")
+	private BigDecimal balance;
+
+	@JsonProperty("created_at")
+	@ApiModelProperty(notes = "The date and time when the balance was collected.")
+	private LocalDateTime creationTimestamp;
+
+}
