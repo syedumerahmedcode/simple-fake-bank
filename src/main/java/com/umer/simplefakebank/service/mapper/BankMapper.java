4a5f94d284b38165e379dd45a1371d0a9891dc78
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-13 22:00:34 +0200

    simple-fake-bank: Added further mapper methods in BankMapper class.

--- src/main/java/com/umer/simplefakebank/service/mapper/BankMapper.java
+++ src/main/java/com/umer/simplefakebank/service/mapper/BankMapper.java
@@ -1,20 +1,25 @@
 package com.umer.simplefakebank.service.mapper;
 
 import com.umer.simplefakebank.dto.request.RequestAccountDTO;
+import com.umer.simplefakebank.dto.request.RequestOperationDTO;
 import com.umer.simplefakebank.dto.response.ResponseAccountDTO;
+import com.umer.simplefakebank.dto.response.ResponseOperationDTO;
 import com.umer.simplefakebank.entities.Account;
+import com.umer.simplefakebank.entities.Operation;
 
 public class BankMapper {
 	
 	public static Account toAccountEntity(RequestAccountDTO requestAccountDTO) {
-		return Account.builder()
+		return Account
+				.builder()
 				.initialDepositAmount(requestAccountDTO.getInitailDeposotAmount())
 				.balance(requestAccountDTO.getInitailDeposotAmount())
 				.build();
 	}
 	
 	public static ResponseAccountDTO toResponseAccountDTO(Account account) {
-		return ResponseAccountDTO.builder()
+		return ResponseAccountDTO
+				.builder()
 				.id(account.getId())
 				.userId(account.getUser().getId())
 				.balance(account.getBalance())
@@ -22,6 +27,24 @@ public class BankMapper {
 				.build();				
 	}
 	
+
+	public static Operation toOperationEntity(RequestOperationDTO requestOperationDTO) {
+		return Operation
+				.builder()
+				// TODO: Check why sender and receiver are not needed here?
+				.value(requestOperationDTO.getValue())
+				.build();
+	}
 	
+	public static ResponseOperationDTO toResponseOperationDTO(Operation operation) {
+		return ResponseOperationDTO
+				.builder()
+				.id(operation.getId())
+				.senderAccountId(operation.getSenderAccount().getId())
+				.receiverAccountId(operation.getReceiverAccount().getId())
+				.value(operation.getValue())
+				.creationTimestamp(operation.getOperationDateTime())
+				.build();		
+	}
 
 }
