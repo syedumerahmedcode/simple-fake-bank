e9cec603a7bfe354eb9d26b9b060460dd7cb20a5
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 20:41:00 +0200

    feature: Created scaffolding for Operation Controller class.

--- src/main/java/com/umer/simplefakebank/controller/OperationController.java
+++ src/main/java/com/umer/simplefakebank/controller/OperationController.java
@@ -1,7 +1,16 @@
 package com.umer.simplefakebank.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umer.simplefakebank.dto.request.RequestOperationDTO;
+import com.umer.simplefakebank.dto.response.ResponseOperationsDTO;
 import com.umer.simplefakebank.service.OperationService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +23,18 @@ public class OperationController {
 	public static final String OPERATION_GET_END_POINT = "/fromAccount/{accountId}";
  
 	private final OperationService operationService;
-
+	
+	
+	@GetMapping(path = "/tbd")
+	public ResponseEntity<ResponseOperationsDTO> getOperations(@PathVariable("accountId") final long accountId){
+		return null;
+	}
+	
+	@PostMapping(path = "/tbd")
+	public ResponseEntity reateNewOperation(@Valid @RequestBody RequestOperationDTO requestOperationDTO) {
+		return null;
+	}
+	
+	
+	
 }
