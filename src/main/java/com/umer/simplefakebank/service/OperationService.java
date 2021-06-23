f85a1e2c22bb66d1f0105cf4ba9f1739c7325262
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 22:56:07 +0200

    feature: Added business logic for retreiving operations.

--- src/main/java/com/umer/simplefakebank/service/OperationService.java
+++ src/main/java/com/umer/simplefakebank/service/OperationService.java
@@ -1,5 +1,10 @@
 package com.umer.simplefakebank.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
@@ -7,7 +12,9 @@ import org.springframework.stereotype.Service;
 import com.umer.simplefakebank.dto.request.RequestOperationDTO;
 import com.umer.simplefakebank.dto.response.ResponseOperationDTO;
 import com.umer.simplefakebank.dto.response.ResponseOperationsDTO;
+import com.umer.simplefakebank.entities.Account;
 import com.umer.simplefakebank.repsitory.OperationRepository;
+import com.umer.simplefakebank.service.mapper.BankMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -28,9 +35,27 @@ public class OperationService {
 	public ResponseOperationsDTO retrieveOperations(long accountId) {
 		log.debug("Retriving operations for the account: {}", accountId);
 		
-		return null;
+		//fetch the account for the given accountId
+		// TODO: Create a method out of it for cleaner code.
+		Account account = accountService.getAccountById(accountId);
+		List<ResponseOperationDTO> operationDTOList = operationRepository
+				.findAllBySenderAccount_IdOrReceiverAccount_IdOrderByOperationDateTimeDesc(account.getId(),
+						account.getId())
+				.orElse(new ArrayList<>()).stream().map(BankMapper::toResponseOperationDTO)
+				.collect(Collectors.toList());
+		return ResponseOperationsDTO.builder()
+				.accountId(accountId)
+				.operationDTOList(operationDTOList)
+				.creationTimestamp(getCurrentTimestamp())
+				.build();
+				
+		
 	}
 	
+	
+	LocalDateTime getCurrentTimestamp() {
+		return LocalDateTime.now();
+	}
 	
 
 }
