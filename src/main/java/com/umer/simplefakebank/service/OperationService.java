dd0bdfb1e0800ce6fa0d87dc78dd60e0acfdd3f7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 20:23:18 +0200

    feature: Added scafolding for Operation service.

--- src/main/java/com/umer/simplefakebank/service/OperationService.java
+++ src/main/java/com/umer/simplefakebank/service/OperationService.java
@@ -1,6 +1,13 @@
 package com.umer.simplefakebank.service;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Service;
+
+import com.umer.simplefakebank.dto.request.RequestOperationDTO;
+import com.umer.simplefakebank.dto.response.ResponseOperationDTO;
+import com.umer.simplefakebank.dto.response.ResponseOperationsDTO;
+import com.umer.simplefakebank.repsitory.OperationRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -9,5 +16,19 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @RequiredArgsConstructor
 public class OperationService {
+	
+	private final AccountService accountService;
+	private final OperationRepository operationRepository;
+	
+	
+	public ResponseOperationDTO createNewOperation(@Valid RequestOperationDTO requestOperationDTO) {
+		return null;
+	}
+	
+	public ResponseOperationsDTO retrieveOperations(long account) {
+		return null;
+	}
+	
+	
 
 }
