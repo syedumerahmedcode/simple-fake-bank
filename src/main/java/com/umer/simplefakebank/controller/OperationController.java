d6c0bc8316c5bf347c36de6d60aa4491f5209753
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 23:04:56 +0200

    feature: Added some java doc.

--- src/main/java/com/umer/simplefakebank/controller/OperationController.java
+++ src/main/java/com/umer/simplefakebank/controller/OperationController.java
@@ -13,7 +13,19 @@ import com.umer.simplefakebank.dto.request.RequestOperationDTO;
 import com.umer.simplefakebank.dto.response.ResponseOperationsDTO;
 import com.umer.simplefakebank.service.OperationService;
 
+import org.springframework.http.MediaType;
 import lombok.RequiredArgsConstructor;
+
+/**
+ * The operation controller exposes two endpoints:
+ *  - GET
+ *  	When account Id is given, it retrieves all of the operations.
+ *  - POST
+ *  	It creates a new transfer operation in your body the sender and receiver accounts ids and the value used
+ *      in the transaction.
+ * @author umer
+ *
+ */
 
 @RestController
 @RequiredArgsConstructor
@@ -21,20 +33,20 @@ public class OperationController {
 
 	public static final String OPERATION_END_POINT_V1 = "/v1/operations";
 	public static final String OPERATION_GET_END_POINT = "/fromAccount/{accountId}";
- 
+
 	private final OperationService operationService;
-	
-	
-	@GetMapping(path = "/tbd")
-	public ResponseEntity<ResponseOperationsDTO> getOperations(@PathVariable("accountId") final long accountId){
+
+	@GetMapping(
+			path = OPERATION_END_POINT_V1, 
+			produces = MediaType.APPLICATION_JSON_VALUE			
+	)
+	public ResponseEntity<ResponseOperationsDTO> getOperations(@PathVariable("accountId") final long accountId) {
 		return ResponseEntity.ok(operationService.retrieveOperations(accountId));
 	}
-	
+
 	@PostMapping(path = "/tbd")
 	public ResponseEntity reateNewOperation(@Valid @RequestBody RequestOperationDTO requestOperationDTO) {
 		return null;
 	}
-	
-	
-	
+
 }
