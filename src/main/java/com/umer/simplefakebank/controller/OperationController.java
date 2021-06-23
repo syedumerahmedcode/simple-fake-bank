854d0b07a40c2384c2a7bef67c160d5550fa1cbf
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 19:58:30 +0200

    feature: Test commit.

--- src/main/java/com/umer/simplefakebank/controller/OperationController.java
+++ src/main/java/com/umer/simplefakebank/controller/OperationController.java
@@ -12,7 +12,7 @@ public class OperationController {
 
 	public static final String OPERATION_END_POINT_V1 = "/v1/operations";
 	public static final String OPERATION_GET_END_POINT = "/fromAccount/{accountId}";
-
+ 
 	private final OperationService operationService;
 
 }
