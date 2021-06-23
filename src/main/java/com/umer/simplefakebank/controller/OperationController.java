779ab7a14a9596c59c338d3bf5076898703a3acb
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 19:39:06 +0200

    feature: Added OperationController class."

---
+++ src/main/java/com/umer/simplefakebank/controller/OperationController.java
@@ -0,0 +1,11 @@
+package com.umer.simplefakebank.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class OperationController {
+
+}
