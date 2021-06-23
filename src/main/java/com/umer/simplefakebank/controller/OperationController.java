85ce2d72acef2107108d0101825c63e75d32edf1
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-06-23 19:48:08 +0200

    feature: Added OperationService class.

--- src/main/java/com/umer/simplefakebank/controller/OperationController.java
+++ src/main/java/com/umer/simplefakebank/controller/OperationController.java
@@ -2,10 +2,17 @@ package com.umer.simplefakebank.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umer.simplefakebank.service.OperationService;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class OperationController {
+
+	public static final String OPERATION_END_POINT_V1 = "/v1/operations";
+	public static final String OPERATION_GET_END_POINT = "/fromAccount/{accountId}";
+
+	private final OperationService operationService;
 
 }
---
+++ src/main/java/com/umer/simplefakebank/service/OperationService.java
@@ -0,0 +1,13 @@
+package com.umer.simplefakebank.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class OperationService {
+
+}
