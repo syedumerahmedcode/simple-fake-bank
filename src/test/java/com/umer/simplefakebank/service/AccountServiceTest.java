3a18298f533a756a8a095b8834fa024194586070
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-07-04 15:37:19 +0200

    Test: Create an empty test method.

--- src/test/java/com/umer/simplefakebank/service/AccountServiceTest.java
+++ src/test/java/com/umer/simplefakebank/service/AccountServiceTest.java
@@ -1,9 +1,31 @@
 package com.umer.simplefakebank.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.umer.simplefakebank.repsitory.AccountReposoitory;
+import com.umer.simplefakebank.repsitory.UserRepository;
 
 @ExtendWith(SpringExtension.class)
 public class AccountServiceTest {
+
+	@Mock
+	private AccountReposoitory accountReposoitory;
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@Spy 
+	@InjectMocks
+	private AccountService accountService;
+	
+	@Test
+	void testCreateNewAccountSuccessfully() {
+		
+	}
 
 }
